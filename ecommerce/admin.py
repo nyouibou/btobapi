e028afe3a3183b4cad3dfb583fe1275f48242ef6
nyouibou <akshaits4@gmail.com>
2024-12-02 12:44:55 +0530

    intial update

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product,Category,Order,BusinessUser
 
-# Register your models here.
+# Register your models here
+admin.site.register(BusinessUser)
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Order)
