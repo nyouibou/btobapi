379e1cab6e1721cb0d9c983f050522307927f468
nyouibou <akshaits4@gmail.com>
2024-12-02 13:01:42 +0530

    updated dashboard

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Product,Category,Order,BusinessUser
+from .models import Product,Category,Order,BusinessUser,Offer
 
 # Register your models here
 admin.site.register(BusinessUser)
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Order)
+admin.site.register(Offer)
+
