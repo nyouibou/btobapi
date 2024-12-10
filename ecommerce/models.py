74763d56250123915407aef5543e9e5d7a2e65a6
nyouibou <akshaits4@gmail.com>
2024-12-10 11:32:37 +0530

    apiupgrade

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -4,26 +4,22 @@ from django.db import models
 class BusinessUser(models.Model):
     company_name = models.CharField(max_length=255)
     contact_person = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15)
-    address = models.TextField()
-    uploaded_file = models.FileField(upload_to='business_user_files/', blank=True, null=True)
-    referral_code = models.CharField(max_length=50, blank=True, null=True)  # Optional referral code
-    cashback_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)  # Cashback amount
+    uploaded_file = models.FileField(upload_to='business_user_files/', blank=True, null=True)  # Optional field
+    referral_code = models.CharField(max_length=50, blank=True, null=True)
+    cashback_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
     def __str__(self):
         return self.company_name
 
     def apply_referral_cashback(self, total_order_amount):
-        """
-        Apply a 5% cashback if the referral code is "leafcoin".
-        """
         if self.referral_code == "leafcoin":
             cashback = total_order_amount * 0.05
-            self.cashback_amount = ('cashback_amount') + cashback
+            self.cashback_amount += cashback
             self.save()
             return cashback
         return 0
+
 
 
 class Offer(models.Model):
@@ -81,10 +77,8 @@ class Order(models.Model):
     business_user = models.ForeignKey(BusinessUser, related_name="orders", on_delete=models.CASCADE)
     order_date = models.DateField(auto_now_add=True)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    shipping_address = models.TextField()
     billing_address = models.TextField()
     status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='Pending')
-    payment_terms = models.CharField(max_length=50)
     order_type = models.CharField(max_length=50, choices=ORDER_TYPE_CHOICES)
     cashback_applied = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)  # Track cashback applied
 
@@ -105,8 +99,6 @@ class OrderProduct(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    wholesale_price = models.DecimalField(max_digits=10, decimal_places=2)
-    total = models.DecimalField(max_digits=10, decimal_places=2)
 
     def save(self, *args, **kwargs):
         # Ensure enough stock is available
